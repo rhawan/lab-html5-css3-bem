f8d5c8f016350a811ecc1e734aa120d4df8a5d9a
Rhawan Brenner <rb.brenner10@gmail.com>
2017-10-09 23:10:52 -0300

    Workshop html5 com css3

---
+++ projeto-curso/javascripts/app.js
@@ -0,0 +1,5 @@
+var alternador = document.querySelector('.js-botao-chaveador');
+alternador.onclick = function() {
+	var menu = document.querySelector('.js-menu');
+	menu.classList.toggle('esta-exibindo');
+}
